4c825764eb6746d89e20b7a9110fe79f2d671e3d
Metidji Sid Ahmed <is_metidji@esi.dz>
2020-09-22 00:01:27 +0200

    the comments are in dishId

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -73,7 +73,7 @@ class Main extends Component {
         <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id == match.params.dishId)[0]}
           isLoading={this.props.dishes.isLoading}
           errMess={this.props.dishes.errMess}
-          comments={this.props.comments.comments.filter((comment) => comment.dish == match.params.dishId)}
+          comments={this.props.comments.comments.filter((comment) => comment.dishId == match.params.dishId)}
           commentsErrMess={this.props.comments.errMess}
           postComment={this.props.postComment}
           favorite={this.props.favorites.favorites ?  this.props.favorites.favorites.dishes.some((dish) => {return dish.id == match.params.dishId} ) : null}
@@ -83,7 +83,7 @@ class Main extends Component {
         <DishDetail dish={this.props.dishes.dishes.filter((dish) => dish.id == match.params.dishId)[0]}
           isLoading={this.props.dishes.isLoading}
           errMess={this.props.dishes.errMess}
-          comments={this.props.comments.comments.filter((comment) => comment.dish == match.params.dishId)}
+          comments={this.props.comments.comments.filter((comment) => comment.dishId == match.params.dishId)}
           commentsErrMess={this.props.comments.errMess}
           postComment={this.props.postComment}
           favorite={false}
